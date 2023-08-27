c386d2449ca7e207a4242d3b28fae3a9016ba249
Majed <boumaged@gmail.com>
2023-08-27 14:46:08 +0400

    Initial Commit

---
+++ TabbyCat/TabbyCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbyCatApp.swift
+//  TabbyCat
+//
+//  Created by Majed on 27/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TabbyCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
